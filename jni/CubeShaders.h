867a07a199c8ac45d2ab4968571b6442a1d49f13
Enes Poyraz <epoyraz.eth@googlemail.com>
2012-12-03 12:47:53 +0100

    Cube with color

--- jni/CubeShaders.h
+++ jni/CubeShaders.h
@@ -20,10 +20,10 @@ static const char* cubeMeshVertexShader = " \
   \
 attribute vec4 vertexPosition; \
 attribute vec4 vertexNormal; \
-attribute vec2 vertexTexCoord; \
- \
-varying vec2 texCoord; \
+attribute vec4 aVertexColor;\
+\
 varying vec4 normal; \
+varying vec4 frontColor;\
  \
 uniform mat4 modelViewProjectionMatrix; \
  \
@@ -31,91 +31,19 @@ void main() \
 { \
    gl_Position = modelViewProjectionMatrix * vertexPosition; \
    normal = vertexNormal; \
-   texCoord = vertexTexCoord; \
+   frontColor = aVertexColor;\
 } \
 ";
+
 
 static const char* cubeFragmentShader = " \
  \
 precision mediump float; \
  \
-varying vec2 texCoord; \
-varying vec4 normal; \
- \
-uniform sampler2D texSampler2D; \
- \
-\
 varying vec4 frontColor;\
-varying vec4 pos;\
-\
-uniform sampler2D uBackCoord;\
-uniform sampler2D uVolData;\
-uniform sampler2D uTransferFunction;\
-\
-const float steps = 50.0;\
-const float numberOfSlices = 96.0;\
-const float slicesOverX = 10.0;\
-const float slicesOverY = 10.0;\
-\
-\
-float getVolumeValue(vec3 volpos)\
-{\
-	float s1,s2;\
-	float dx1,dy1;\
-	float dx2,dy2;\
-\
-	vec2 texpos1,texpos2;\
-\
-	s1 = floor(volpos.z*numberOfSlices);\
-	s2 = s1+1.0;\
-\
-	dx1 = fract(s1/slicesOverX);\
-	dy1 = floor(s1/slicesOverY)/slicesOverY;\
-\
-	dx2 = fract(s2/slicesOverX);\
-	dy2 = floor(s2/slicesOverY)/slicesOverY;\
-\
-	texpos1.x = dx1+(volpos.x/slicesOverX);\
-	texpos1.y = dy1+(volpos.y/slicesOverY);\
-\
-	texpos2.x = dx2+(volpos.x/slicesOverX);\
-	texpos2.y = dy2+(volpos.y/slicesOverY);\
-\
-	return mix( texture2D(uVolData,texpos1).x, texture2D(uVolData,texpos2).x, (volpos.z*numberOfSlices)-s1);\
-}\
-\
 void main() \
 { \
-   	vec2 texC = pos.xy/pos.w;\
-	texC.x = 0.5*texC.x + 0.5;\
-	texC.y = 0.5*texC.y + 0.5;\
-	vec4 backColor = texture2D(uBackCoord,texC);\
-\
-	vec3 dir = backColor.rgb - frontColor.rgb;\
-	vec4 vpos = frontColor;\
-\
-	float cont = 0.0;\
-\
-	vec3 Step = dir/steps;\
-\
-	vec4 accum = vec4(0, 0, 0, 0);\
-	vec4 sample = vec4(0.0, 0.0, 0.0, 0.0);\
-	vec4 value = vec4(0, 0, 0, 0);\
-\
-	float opacityFactor = 8.0;\
-	float lightFactor = 1.3;\
-	for(int i=0;i<50;i+=1)\
-	{\
-		vec2 tf_pos;\
-		tf_pos.x = getVolumeValue(vpos.xyz);\
-		tf_pos.y = 0.5;\
-		value = vec4(tf_pos.x);\
-		sample.a = value.a * opacityFactor * (1.0/steps);\
-		sample.rgb = value.rgb * sample.a * lightFactor;\
-		accum.rgb += (1.0 - accum.a) * sample.rgb;\
-		accum.a += sample.a;\
-	}\
-	gl_FragColor = vec4(0.0, 1.0, 1.0, 0.5);\
+   gl_FragColor = frontColor; \
 } \
 ";
 
--- jni/ImageTargets.cpp
+++ jni/ImageTargets.cpp
@@ -66,9 +66,11 @@ unsigned int backgroundTexture  = 0;
 unsigned int shaderProgramID    = 0;
 GLint vertexHandle              = 0;
 GLint normalHandle              = 0;
-GLint textureCoordHandle        = 0;
+//GLint textureCoordHandle        = 0;
 GLint mvpMatrixHandle           = 0;
-GLint colorHandle				= 0;
+GLint vertexColorHandle			= 0;
+//GLint projectionMatrixHandle    = 0;
+//GLint modelViewMatrixHandle     = 0;
 #endif
 
 // Screen dimensions:
@@ -400,13 +402,16 @@ Java_edu_ethz_s3d_S3DRenderer_renderFrame(JNIEnv *, jobject)
          
         glVertexAttribPointer(vertexHandle, 3, GL_FLOAT, GL_FALSE, 0,
                               (const GLvoid*) &teapotVertices[0]);
+        glVertexAttribPointer(vertexColorHandle,3,GL_FLOAT, GL_FALSE, 0,
+                (const GLvoid*) &color[0]);
         glVertexAttribPointer(normalHandle, 3, GL_FLOAT, GL_FALSE, 0,
-                              (const GLvoid*) &teapotNormals[0]);
+                             (const GLvoid*) &teapotNormals[0]);
   //      glVertexAttribPointer(textureCoordHandle, 2, GL_FLOAT, GL_FALSE, 0,
   //                            (const GLvoid*) &teapotTexCoords[0]);
-  //      bindTexture(TEXTURE_2D,);
+
         glEnableVertexAttribArray(vertexHandle);
         glEnableVertexAttribArray(normalHandle);
+        glEnableVertexAttribArray(vertexColorHandle);
      //   glEnableVertexAttribArray(textureCoordHandle);
         
       //  glActiveTexture(GL_TEXTURE0);
@@ -414,7 +419,14 @@ Java_edu_ethz_s3d_S3DRenderer_renderFrame(JNIEnv *, jobject)
 
         glUniformMatrix4fv(mvpMatrixHandle, 1, GL_FALSE,
                            (GLfloat*)&modelViewProjection.data[0] );
-        glDrawElements(GL_TRIANGLE_STRIP, NUM_TEAPOT_OBJECT_INDEX, GL_UNSIGNED_SHORT,
+
+//        glUniformMatrix4fv(projectionMatrixHandle, 1, GL_FALSE,
+//                           (GLfloat*)&projectionMatrix.data[0] );
+//
+//        glUniformMatrix4fv(modelViewMatrixHandle, 1, GL_FALSE,
+//                           (GLfloat*)&modelViewMatrix.data[0] );
+
+        glDrawElements(GL_TRIANGLES, NUM_TEAPOT_OBJECT_INDEX, GL_UNSIGNED_SHORT,
                        (const GLvoid*) &teapotIndices[0]);
 
         SampleUtils::checkGlError("ImageTargets renderFrame");
@@ -428,11 +440,13 @@ Java_edu_ethz_s3d_S3DRenderer_renderFrame(JNIEnv *, jobject)
     glDisable(GL_TEXTURE_2D);
     glDisableClientState(GL_VERTEX_ARRAY);
     glDisableClientState(GL_NORMAL_ARRAY);
-    glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+    //glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+    glDisableClientState(GL_COLOR_ARRAY);
 #else
     glDisableVertexAttribArray(vertexHandle);
     glDisableVertexAttribArray(normalHandle);
-    glDisableVertexAttribArray(textureCoordHandle);
+    //glDisableVertexAttribArray(textureCoordHandle);
+    glDisableVertexAttribArray(vertexColorHandle);
 #endif
 
     QCAR::Renderer::getInstance().end();
@@ -719,10 +733,19 @@ Java_edu_ethz_s3d_S3DRenderer_initRendering(JNIEnv* env, jobject obj)
                                                 "vertexPosition");
     normalHandle        = glGetAttribLocation(shaderProgramID,
                                                 "vertexNormal");
-    textureCoordHandle  = glGetAttribLocation(shaderProgramID,
-                                                "vertexTexCoord");
+//    textureCoordHandle  = glGetAttribLocation(shaderProgramID,
+//                                                "vertexTexCoord");
     mvpMatrixHandle     = glGetUniformLocation(shaderProgramID,
                                                 "modelViewProjectionMatrix");
+
+    //vertexHandle        		= glGetAttribLocation(shaderProgramID,
+    //                                            "aVertexPosition");
+    vertexColorHandle         	= glGetAttribLocation(shaderProgramID,
+                                                "aVertexColor");
+    //projectionMatrixHandle      = glGetAttribLocation(shaderProgramID,
+    //											"uPMatrix");
+    //modelViewMatrixHandle		= glGetAttribLocation(shaderProgramID,
+    //											"uMVMatrix");
 
 #endif
 
